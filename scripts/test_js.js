a09f10f0f58beaa376f9f21d0d32525cd6aa7266
shurns <shburns02@gmail.com>
2015-01-13 23:33:09 -0500

    Inserting test to determine functionality of javascript on GitHub pages. Because I could look it up on google, or I could fiddle with it on my own. And I chose the latter.

---
+++ scripts/test_js.js
@@ -0,0 +1,7 @@
+﻿//***************************************************
+// Testing the functionality of js on GitHub pages
+//***************************************************
+
+var testFunction = function () {
+	console.log("GitHub pages supports loading and using JS.");
+};
